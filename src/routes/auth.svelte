05104e71cde1577a52d1d47716090d7ea5995bc7
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-01-21 21:48:33 +0000

    feat: get Github Api

--- src/routes/auth.svelte
+++ src/routes/auth.svelte
@@ -14,7 +14,6 @@
     if (res.ok) {
       const data = await res.json();
       user.set(data.user);
-      console.log($user);
       goto('/');
     }
   };
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,38 @@
 <script lang="ts" context="module">
   import type { Load } from '@sveltejs/kit';
 
-  export const load: Load = async ({ session }) => {
+  export const load: Load = async ({ session, fetch }) => {
     if (!session.token) {
       return {
         status: 302,
         redirect: 'auth',
       };
     }
-    return {};
+
+    const res = await fetch(
+      'https://api.github.com/search/issues?q=is:issue%20is:open%20label:%22good%20first%20issue%22%20org:EddieHubCommunity',
+      {
+        headers: {
+          Authorization: `token ${session.token}`,
+        },
+      },
+    );
+
+    if (res.ok) {
+      const data = await res.json();
+      return {
+        props: {
+          issues: data.items,
+        },
+      };
+    }
   };
+</script>
+
+<script lang="ts">
+  export let issues: any[];
 </script>
 
 <h1>Welcome to Issue-Crawler</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{JSON.stringify(issues)}
