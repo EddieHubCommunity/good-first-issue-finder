461c9ce01d99dc73348535a7d4c61f3bb0949a6b
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-01-22 22:37:50 +0000

    feat: adjust card layout

--- src/lib/components/issue-card.svelte
+++ src/lib/components/issue-card.svelte
@@ -6,11 +6,19 @@
 </script>
 
 <Card>
-  <a
-    class="transition duration-100 underline hover:no-underline"
-    href={issue.html_url}
-    target="_blank"
-  >
-    {issue.title}</a
-  >
+  <div class="flex items-center justify-between">
+    <div class="flex items-center">
+      <img class="object-contain mr-4" src="/images/githubmark.png" alt="github" />
+      <a
+        class="transition duration-100 underline hover:no-underline mr-4"
+        href={issue.html_url}
+        target="_blank"
+      >
+        {issue.title}</a
+      >
+    </div>
+    <a href={`https://gitpod.io/#${issue.html_url}`} target="_blank">
+      <img src="/images/open-in-gitpod.svg" alt="open-in-gitpod" /></a
+    >
+  </div>
 </Card>
