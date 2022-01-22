9696727f01b1649558580e0b6c5e424b21bc0f00
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-01-22 08:19:36 +0000

    feat: update layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,9 +3,7 @@
   import Nav from '$lib/components/nav.svelte';
 </script>
 
-<div class="flex flex-col min-h-screen">
-  <div class="w-[1400px] mx-auto my-0">
-    <Nav />
-    <slot />
-  </div>
+<div class="flex flex-col min-h-screen max-w-[1400px] w-[90%] mx-auto my-0 float-none">
+  <Nav />
+  <slot />
 </div>
