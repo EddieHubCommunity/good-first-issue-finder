98a293f1ef2fefc588c531500987cc924ee285d0
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-01-23 16:06:39 +0000

    feat: add backlink to nav

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,3 +1,5 @@
 <nav class="flex items-center justify-between py-6">
-  <img src="/images/hubber.png" class="object-contain w-12" alt="hubber" />
+  <a href="/">
+    <img src="/images/hubber.png" class="object-contain w-12" alt="hubber" />
+  </a>
 </nav>
