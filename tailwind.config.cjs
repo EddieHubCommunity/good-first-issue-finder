cbb303ac596db8ee98b0f427cb0c7eac41f6b070
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-01-23 16:33:01 +0000

    feat: add button component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,14 @@ const config = {
     extend: {
       colors: {
         background: '#f5f4f4',
+        eddiehub: {
+          100: '#ffb953',
+          200: '#ff5a00',
+          300: '#c23100',
+        },
+      },
+      dropShadow: {
+        standard: '0 1rem 2rem 0 rgba(0, 0, 0, 0.05)',
       },
     },
   },
