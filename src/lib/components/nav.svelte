1f5c1045014514dda57cb1fc1aa573e7a730c606
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-01-23 17:33:51 +0100

    feat: add button component (#21)

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,5 +1,10 @@
+<script lang="ts">
+  import Button from './button.svelte';
+</script>
+
 <nav class="flex items-center justify-between py-6">
   <a href="/">
     <img src="/images/hubber.png" class="object-contain w-12" alt="hubber" />
   </a>
+  <Button label="Sign Out" variant="primary" />
 </nav>
