867dea7297d2d2d205f9cb3ec6fa9cc0a11c7add
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-01-25 17:11:03 +0000

    feat: add node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,32 +1,32 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		vite: {
-			server: {
-				hmr: {
-					clientPort: process.env.HMR_HOST ? 443 : 3000,
-					host: process.env.HMR_HOST
-						? process.env.HMR_HOST.substring('https://'.length)
-						: 'localhost'
-				}
-			}
-		}
-	}
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    vite: {
+      server: {
+        hmr: {
+          clientPort: process.env.HMR_HOST ? 443 : 3000,
+          host: process.env.HMR_HOST
+            ? process.env.HMR_HOST.substring('https://'.length)
+            : 'localhost',
+        },
+      },
+    },
+  },
 };
 
 export default config;
