27ac65e8411b4be6a782b5c6ce02d9a401bb8c5c
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-01-27 10:44:41 +0000

    feat: add hooks

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-  import Button from './button.svelte';
-</script>
-
 <nav class="flex items-center justify-between py-6">
   <a href="/">
     <img src="/images/hubber.png" class="object-contain w-12" alt="hubber" />
