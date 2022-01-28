61af7245725fffe23336bf9211ef535fa5a173e6
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-01-28 18:13:25 +0000

    fix: deployment

--- src/routes/api/get-issues.ts
+++ src/routes/api/get-issues.ts
@@ -2,7 +2,7 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { Octokit } from 'octokit';
 
 export const get: RequestHandler = async () => {
-  const token = import.meta.env.VITE_TOKEN;
+  const token = process.env['VITE_TOKEN'];
   if (!token) return { status: 500, body: { message: 'please provide a token' } };
   const octokit = new Octokit({ auth: token });
   const response = await octokit.request('GET /search/issues', {
