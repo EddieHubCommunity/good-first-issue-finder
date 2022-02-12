8ba650fe168af09a0bd9506f3cc3af7f0716efeb
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-02-12 17:01:27 +0100

    feat: add Search (#58)

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -1,9 +1,3 @@
-<div class="card rounded-xl bg-white p-4">
+<div class="card rounded-xl bg-white p-4 shadow-standard">
   <slot />
 </div>
-
-<style lang="postcss">
-  .card {
-    box-shadow: rgba(0, 0, 0, 0.05) 0px 1rem 2rem;
-  }
-</style>
