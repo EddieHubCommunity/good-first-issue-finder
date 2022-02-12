f265f83cf5d47349d6f7eb862c39b766df51c304
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-02-12 23:10:10 +0100

    style: Update Card Layout (#65)

--- src/lib/components/issue-card.svelte
+++ src/lib/components/issue-card.svelte
@@ -2,33 +2,60 @@
   import type { Node } from '../../global';
   import Card from './card.svelte';
   import Label from './label.svelte';
+  import RepoHeader from './repo-header.svelte';
 
   export let issue: Node;
+  let isToggled = true;
+
+  const handleToggle = () => {
+    isToggled = !isToggled;
+  };
 </script>
 
 <Card>
   <div class="flex items-center justify-between">
-    <div class="flex items-center">
+    <div class="flex min-w-0 items-center">
       <img class="mr-4 object-contain" src="/images/githubmark.svg" width="32" alt="github" />
-      <a
-        class="mr-4 underline transition duration-100 hover:no-underline"
-        href={issue.url}
-        target="_blank"
-      >
-        {issue.title}</a
+      <div class="truncate">
+        <RepoHeader
+          class="mr-1 hidden md:inline-block"
+          owner={issue.repository.owner.login}
+          repo={issue.repository.name}
+          repoLink={issue.repository.url}
+        />
+        <a
+          class="min-w-0 truncate font-bold transition-all duration-200 hover:text-eddiehub-200 focus:text-eddiehub-200"
+          href={issue.url}
+          target="_blank"
+        >
+          {issue.title}</a
+        >
+      </div>
+    </div>
+    <div class="flex flex-shrink-0">
+      <a class="hidden sm:block" href={`https://gitpod.io/#${issue.url}`} target="_blank">
+        <img class="object-fill" src="/images/open-in-gitpod.svg" alt="open-in-gitpod" /></a
+      ><button on:click={() => handleToggle()} class="px-2 md:px-4">
+        <img
+          src="/images/collapse.svg"
+          alt="collapse"
+          class="object-fill transition-all duration-200 lg:w-4"
+          class:rotate={!isToggled}
+        /></button
       >
     </div>
-    <a
-      class="hidden flex-shrink-0 sm:block"
-      href={`https://gitpod.io/#${issue.url}`}
-      target="_blank"
-    >
-      <img class="object-fill" src="/images/open-in-gitpod.svg" alt="open-in-gitpod" /></a
-    >
   </div>
-  <div class="mt-2 space-y-2">
-    {#each issue.labels.edges as label}
-      <Label text={label.node.name} color={label.node.color} />
-    {/each}
-  </div>
+  {#if !isToggled}
+    <div class="mt-2 space-y-2">
+      {#each issue.labels.edges as label}
+        <Label text={label.node.name} color={label.node.color} />
+      {/each}
+    </div>
+  {/if}
 </Card>
+
+<style lang="postcss">
+  .rotate {
+    @apply rotate-180;
+  }
+</style>
---
+++ src/lib/components/repo-header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let className = '';
+  export let repo: string;
+  export let repoLink = '';
+  export let owner: string;
+  export { className as class };
+</script>
+
+<div>
+  <a
+    target="_blank"
+    class="text-neutral-500 transition-all duration-200 hover:text-eddiehub-200 focus:text-eddiehub-200 {className}"
+    href={repoLink}>{owner}/{repo}</a
+  >
+</div>
