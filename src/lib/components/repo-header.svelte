897a705d844c2d1bb2b4b0d5d481802eeab94728
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-02-12 21:54:18 +0000

    style: update card layout

---
+++ src/lib/components/repo-header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let className = '';
+  export let repo: string;
+  export let repoLink = '';
+  export let owner: string;
+  export { className as class };
+</script>
+
+<a class={className} href={repoLink}>{owner}/{repo}</a>
