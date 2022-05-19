72add372f60b16460dec7df298b4fa8e2bbfc673
CHIRAG LAL <79036453+chiraglal@users.noreply.github.com>
2022-05-19 21:58:53 +0530

    feat: Added Discord and GitHub icons in footer instead of the text. (#130)

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -4,13 +4,15 @@
       <a
         class="transition duration-200 hover:text-eddiehub-200 active:text-eddiehub-200"
         href="https://discord.com/invite/jZQs6Wu"
-        target="_blank">Discord</a
+        target="_blank"
+        aria-label="Discord"><i class="fa-brands fa-discord" /></a
       >
     </li>
     <li>
       <a
         class="transition duration-200 hover:text-eddiehub-200 active:text-eddiehub-200"
-        href="http://github.com/EddieHubCommunity">Github</a
+        href="http://github.com/EddieHubCommunity"
+        aria-label="Github"><i class="fa-brands fa-github" /></a
       >
     </li>
   </ul>
