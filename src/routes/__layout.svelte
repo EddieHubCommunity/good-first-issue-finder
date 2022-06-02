77cca84ba6e2f3cc86ebde1d9a44a30516f28e55
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-02 17:57:50 +0300

    move div inside header

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,9 +7,6 @@
 <div class="flex min-h-screen flex-col">
   <div class="float-none mx-auto my-0 w-[90%] max-w-[1400px]">
     <Nav />
-    <div class="mb-8 text-center">
-      Eddiehub Issue-Crawler for finding <b>good-first-issues</b>
-    </div>
     <slot />
   </div>
   <Footer />
