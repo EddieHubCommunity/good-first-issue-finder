866cc5f8b55c69415782c2ca6a91c42609676303
Dharmik_403 <64529217+Dharmik48@users.noreply.github.com>
2022-07-02 21:36:15 +0530

    fix: remove navbar (#165)

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,10 @@
 <script>
   import '../app.css';
-  import Nav from '$lib/components/nav.svelte';
   import Footer from '$lib/components/footer.svelte';
 </script>
 
 <div class="flex min-h-screen flex-col">
   <div class="float-none mx-auto my-0 w-[90%] max-w-[1400px]">
-    <Nav />
     <slot />
   </div>
   <Footer />
