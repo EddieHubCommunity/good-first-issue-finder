ba79055e265ad0746900d5eec4b0927f962c0c21
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-08-19 09:31:51 +0200

    feat: remove env-cmd (#186)

--- src/routes/api/get-issues/+server.ts
+++ src/routes/api/get-issues/+server.ts
@@ -1,12 +1,13 @@
 import { json as json$1 } from '@sveltejs/kit';
 import type { RequestHandler } from '@sveltejs/kit';
 import { Octokit } from 'octokit';
+import { env } from '$env/dynamic/private';
 import type { SearchResponse } from '../../../global';
 
 type Response = { search: SearchResponse };
 
 export const POST: RequestHandler = async ({ request }) => {
-  const token = process.env['GH_TOKEN'];
+  const token = env.GH_TOKEN;
   if (!token)
     return json$1(
       { message: 'please provide a token' },
