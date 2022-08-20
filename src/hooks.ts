7f888cd44597be038008de337157f50c4c9f38ae
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-08-20 15:48:09 +0000

    fix: handling empty cookies

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,7 +2,7 @@ import type { Handle } from '@sveltejs/kit';
 import cookie from 'cookie';
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const cookies = cookie.parse(event.request.headers.get('cookie') || '');
+  const cookies = cookie.parse(event.request.headers.get('cookie') || '') || '';
   event.locals.user = cookies.user;
 
   const response = await resolve(event);
