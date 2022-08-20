4746c08a37e0f0aec18971f123a54719bec7ccce
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-08-20 13:38:49 +0000

    feat: display login state

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,20 +1,27 @@
-<footer class="mt-auto w-full bg-skin-off-background py-4">
-  <ul class="flex list-none items-center justify-center gap-8">
-    <li>
-      <a
-        class="transition duration-200 hover:text-skin-primary active:text-skin-primary"
-        href="https://discord.com/invite/jZQs6Wu"
-        target="_blank"
-        aria-label="Discord"><i class="fa-brands fa-discord fa-xl" /></a
-      >
-    </li>
-    <li>
-      <a
-        target="_blank"
-        class="transition duration-200 hover:text-skin-primary active:text-skin-primary"
-        href="http://github.com/EddieHubCommunity"
-        aria-label="Github"><i class="fa-brands fa-github fa-xl" /></a
-      >
-    </li>
-  </ul>
+<script lang="ts">
+  import ThemeSwitcher from './theme-switcher.svelte';
+</script>
+
+<footer class="mt-auto  w-full  bg-skin-off-background py-4">
+  <div class="mx-auto flex w-[90%] max-w-[1440px] items-center justify-between">
+    <ThemeSwitcher />
+    <ul class="flex list-none items-center justify-center gap-8">
+      <li>
+        <a
+          class="transition duration-200 hover:text-skin-primary active:text-skin-primary"
+          href="https://discord.com/invite/jZQs6Wu"
+          target="_blank"
+          aria-label="Discord"><i class="fa-brands fa-discord fa-xl" /></a
+        >
+      </li>
+      <li>
+        <a
+          target="_blank"
+          class="transition duration-200 hover:text-skin-primary active:text-skin-primary"
+          href="http://github.com/EddieHubCommunity"
+          aria-label="Github"><i class="fa-brands fa-github fa-xl" /></a
+        >
+      </li>
+    </ul>
+  </div>
 </footer>
