625cf5a7516cb67612729a5d085f94e44c9f9239
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-08-27 10:09:47 +0000

    test: add Placeholder tests (#193)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,26 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { type PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
   webServer: {
     command: 'npm run build && npm run preview',
     port: 4173,
+  },
+  use: {
+    baseURL: 'http://localhost:4173',
   },
 };
 
