008697e23efffa4df00682dced7001534ed996a2
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-08-28 17:58:22 +0000

    feat: add loading animation

--- src/lib/components/load-more.svelte
+++ src/lib/components/load-more.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Loader from './loader.svelte';
   import { createEventDispatcher } from 'svelte';
 
   export let isDisabled = false;
@@ -8,7 +9,10 @@
 <button
   disabled={isDisabled}
   on:click={() => dispatch('load')}
-  class="flex gap-4 rounded-lg border border-gray-500 bg-skin-off-background p-2 transition-all delay-[50] duration-200 hover:shadow-lg dark:hover:shadow-dark"
->
-  Load More
+  class="flex items-center justify-center gap-4 rounded-lg bg-skin-off-background p-2 transition-all delay-[50] duration-200 hover:shadow-lg dark:hover:shadow-dark"
+  >{#if isDisabled}
+    <Loader /> Loading...
+  {:else}
+    Load More
+  {/if}
 </button>
