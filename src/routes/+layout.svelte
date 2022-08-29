01be9abbe4e1e7b601c90b34455df98791c2470b
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-08-29 16:15:57 +0000

    style: width

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <div class="flex min-h-screen flex-col">
-  <div class="float-none mx-auto my-0 w-[90%] max-w-[1400px]">
+  <div class="float-none mx-auto my-0 w-[90%] max-w-[1440px]">
     <Header username={data.username} />
     <slot />
   </div>
