c035e657e0ec1ec569959f6d15f69ac8b5bcb80a
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-08-31 15:58:37 +0100

    Added language filter function.

--- src/lib/components/filter.svelte
+++ src/lib/components/filter.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
   import { selectedLabels } from '$lib/stores/selected-labels.store';
   export let tags: string[];
+  export let lang: any[];
   let selection: string[] = [];
+  let languageList: string[] = [];
+
+  // Null values check.
+  lang.map((language) =>
+    language.node.repository.primaryLanguage !== null
+      ? languageList.push(language.node.repository.primaryLanguage.name)
+      : '',
+  );
+  // Using a Set.
+  languageList = Array.from(new Set(languageList));
 
   $: selection, run();
   const run = () => {
@@ -15,6 +26,13 @@
     <label
       class="my-1 mr-2 inline-block cursor-pointer select-none rounded-xl border-[1px] border-gray-500 py-1 px-2 text-sm transition-all duration-200"
       for={tag}>{@html tag}</label
+    >
+  {/each}
+  {#each languageList as langList}
+    <input class="hidden" value={langList} bind:group={selection} type="checkbox" id={langList} />
+    <label
+      class="my-1 mr-2 inline-block cursor-pointer select-none rounded-xl border-[1px] border-gray-500 py-1 px-2 text-sm transition-all duration-200"
+      for={langList}>{@html langList}</label
     >
   {/each}
 </div>
