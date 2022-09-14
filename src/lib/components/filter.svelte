3d157394ad3c955a550368a1d04b90cec0d9f822
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-09-14 15:28:28 +0000

    fix: responsivness

--- src/lib/components/filter.svelte
+++ src/lib/components/filter.svelte
@@ -10,7 +10,7 @@
   };
 </script>
 
-<ul class="mt-4 grid w-full grid-cols-3">
+<ul class="mt-8 grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
   {#each tags as tag}
     <li>
       <Checkbox bind:group={selection} label={tag} value={tag} />
