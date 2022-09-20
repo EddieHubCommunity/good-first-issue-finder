9295743e7ec77bb8e6feb467367b120c5c9c84ba
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-09-20 12:49:17 +0000

    style: adjust styling

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -6,7 +6,7 @@
   <div class="sticky top-4 hidden w-1/5 self-start lg:block">
     <Menu />
   </div>
-  <div class="w-full lg:w-3/5 lg:border-x lg:border-skin-text lg:px-8">
+  <div class="w-full border-gray-300 dark:border-gray-600 lg:w-3/5 lg:border-x lg:px-8">
     <slot />
   </div>
   <div class="hidden w-1/5  lg:block" />
