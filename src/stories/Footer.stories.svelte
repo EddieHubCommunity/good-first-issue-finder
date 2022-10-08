9e7db2feed9f3ee151e323743d2a31e4d4e36101
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-10-08 20:56:05 +0000

    feat: version story

--- src/stories/Footer.stories.svelte
+++ src/stories/Footer.stories.svelte
@@ -2,6 +2,10 @@
   import { Meta, Story } from '@storybook/addon-svelte-csf';
 
   import Footer from '$lib/components/footer.svelte';
+  import { setContext } from 'svelte';
+  import { versionKey } from '../lib/util/version';
+
+  setContext(versionKey, '1.0.0');
 </script>
 
 <Meta title="Composed/Footer" component={Footer} />
---
+++ src/stories/Version.stories.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+  import { setContext } from 'svelte';
+
+  import Version from '../lib/components/package-version.svelte';
+
+  import { versionKey } from '../lib/util/version';
+
+  setContext(versionKey, '1.0.0');
+</script>
+
+<Meta title="Version" component={Version} />
+
+<Template let:args>
+  <Version {...args} />
+</Template>
+
+<Story name="Default" />
