b47d80dfa2260fcab6057a9ef0d7ff8ff08d3a65
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-10-11 18:50:07 +0000

    chore: add additional config

--- histoire.config.ts
+++ histoire.config.ts
@@ -3,6 +3,16 @@ import { HstSvelte } from '@histoire/plugin-svelte';
 import path from 'path';
 
 export default defineConfig({
+  backgroundPresets: [
+    {
+      label: 'light',
+      color: '#f5f4f4',
+    },
+    {
+      label: 'dark',
+      color: '#22272e',
+    },
+  ],
   setupFile: 'src/histoire-setup.ts',
   plugins: [HstSvelte()],
   vite: {
--- src/stories/label.story.svelte
+++ src/stories/label.story.svelte
@@ -3,12 +3,16 @@
   import type { Hst as HstType } from '@histoire/plugin-svelte';
   export let Hst: HstType;
 
-  const { Story } = Hst;
+  const { Story, Text } = Hst;
 
   let color = '#f343f4';
   let text = 'My Label';
 </script>
 
-<Story title="Labels">
+<Story title="Components/Labels">
+  <svelte:fragment slot="controls">
+    <Text bind:value={color} title="color" />
+    <Text bind:value={text} title="text" />
+  </svelte:fragment>
   <Label {color} {text} />
 </Story>
