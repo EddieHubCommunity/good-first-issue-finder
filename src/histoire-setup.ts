7a50846d7684c1b0dbbea5dc3522f2eb4131a9e3
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-10-14 16:00:43 +0000

    feat: add issue-card story

--- src/histoire-setup.ts
+++ src/histoire-setup.ts
@@ -4,7 +4,6 @@ const link = document.createElement('link');
 link.type = 'text/css';
 link.rel = 'stylesheet';
 
-//link.href = 'http://fonts.googleapis.com/css?family=Oswald&effect=neon';
 document.head.appendChild(link);
 
 link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css';
