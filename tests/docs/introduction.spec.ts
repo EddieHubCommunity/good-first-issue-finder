8757e13b2fd660b0cc433985b8acf10216f2984d
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-10-17 12:42:32 +0000

    test: add new tests

---
+++ tests/docs/introduction.spec.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { expect, test } from '@playwright/test';
+
+test.fixme('docs header element is active', async ({ page }) => {
+  // navigate to the docs landing page /docs
+  // check if the docs link in the page header is having the active class
+});
+
+test.fixme('Correct Page Heading', async ({ page }) => {
+  // navigate to the docs landing page /docs
+  // check if the heading contains Introduction
+});
+
+test.fixme(
+  'is the left-side menu containing an element with the same name as the page headline',
+  async ({ page }) => {
+    // navigate to the docs landing page /docs
+    // check if the Introduction element in the left menu is holding the active class
+  },
+);
+
+test.fixme('is the highlighted item in the left-menu is the correct one', async ({ page }) => {
+  // navigate to the docs landing page /docs
+  // check if the Introduction element in the left menu is holding the active class
+});
