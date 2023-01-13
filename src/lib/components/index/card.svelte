94f28d6f76a6bd47957028a710852fd48e46e877
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-01-13 18:23:40 +0000

    feat: add new card

---
+++ src/lib/components/index/card.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let className = '';
+  export { className as class };
+</script>
+
+<div
+  class="flex min-h-[300px] w-full flex-col items-center rounded-2xl border border-skin-off-background bg-skin-off-background shadow-xl {className}"
+>
+  <slot />
+</div>
--- src/lib/components/index/link.svelte
+++ src/lib/components/index/link.svelte
@@ -8,6 +8,17 @@
   };
 </script>
 
-<a {href} class="rounded-lg {variantMap[variant]} px-4 py-2">
+<a
+  {href}
+  class="relative rounded-lg bg-skin-primary py-2 px-4 text-white shadow-lg transition duration-200 {variantMap[
+    variant
+  ]}"
+>
   <slot />
 </a>
+
+<style>
+  a:hover {
+    box-shadow: rgba(255, 90, 0, 0.5) 0px 1px 40px;
+  }
+</style>
