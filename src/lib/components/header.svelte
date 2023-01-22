0d1f946b79a0dccc7a02c05fc6cb611ed34e64df
Arun Singh Kushwaha <arunkushwaha89765@gmail.com>
2023-01-23 00:15:17 +0530

    Added hamburger menu for smaller devices

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,16 +1,44 @@
 <script lang="ts">
   import ProfilePicture from './profile-picture.svelte';
   import { page } from '$app/stores';
+  import { onMount } from 'svelte';
   export let username: string;
 
   const navItems = [{ name: 'Docs', path: '/docs' }];
+
+  onMount(() => {
+    const burger = document.querySelector('.navbar-burger');
+    const menu = document.querySelector('.navbar-menu');
+    const closeButton = document.querySelector('.navbar-close');
+    const closeHamburgerMenu = () => {
+      menu?.classList.add('hidden');
+    };
+
+    burger?.addEventListener('click', () => {
+      menu?.classList.toggle('hidden');
+    });
+
+    closeButton?.addEventListener('click', closeHamburgerMenu);
+  });
 </script>
 
 <header class="my-8 flex flex-wrap items-center justify-between">
   <a href="/">
     <img src="/images/hubber.png" class="w-8 object-contain md:w-12" alt="hubber" />
   </a>
-  <div class="flex items-center gap-4 lg:gap-8">
+  <div class="lg:hidden">
+    <button class="navbar-burger flex items-center p-3 text-neutral-900">
+      <svg
+        class="block h-4 w-4 fill-current"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <title>Mobile menu</title>
+        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+      </svg>
+    </button>
+  </div>
+  <div class="hidden items-center gap-4 lg:flex lg:gap-8">
     <ul>
       {#each navItems as item}
         <li>
@@ -31,6 +59,58 @@
         href="/login">Login</a
       >
     {/if}
+  </div>
+
+  <!-- hamburger menu for smaller devices(below lg)-->
+  <div class="navbar-menu absolute z-50 hidden">
+    <div class="navbar-backdrop fixed inset-0 bg-gray-800 opacity-50" />
+    <nav
+      class="fixed top-0 left-0 bottom-0 flex w-5/6 max-w-sm flex-col overflow-y-auto bg-neutral-100 py-6 px-6"
+    >
+      <div class="mb-8 flex items-center justify-between">
+        <a href="/">
+          <img src="/images/hubber.png" class="w-8 object-contain md:w-12" alt="hubber" />
+        </a>
+        <button class="navbar-close">
+          <svg
+            class="h-6 w-6 cursor-pointer text-gray-400 text-neutral-900"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
+      <div class="flex flex-col items-center gap-4 gap-8 ">
+        <ul>
+          {#each navItems as item}
+            <li>
+              <a
+                class="default-transition text-skin-text-highlight decoration-transparent hover:underline hover:decoration-inherit"
+                class:active={$page.url.pathname.includes(item.path)}
+                href={item.path}>{item.name}</a
+              >
+            </li>
+          {/each}
+        </ul>
+        {#if username}
+          <ProfilePicture {username} />
+        {:else}
+          <a
+            data-test-id="login-btn"
+            class="default-transition rounded-xl bg-skin-text px-4 py-2 text-skin-off-background hover:bg-primary-100 dark:bg-skin-text-highlight dark:text-skin-background dark:hover:bg-primary-100"
+            href="/login">Login</a
+          >
+        {/if}
+      </div>
+    </nav>
   </div>
 </header>
 
