c19c078cb657d935423eda34136be7504ae06e18
dhruvin <dhruvin.200410116086@gmail.com>
2023-02-13 11:55:45 +0530

    Update footer.svelte

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,33 +1,38 @@
 <script lang="ts">
   import ThemeSwitcher from './theme-switcher.svelte';
   import PackageVersion from './package-version.svelte';
+  
 </script>
 
 <footer class="mt-auto w-full bg-skin-off-background py-4">
   <div class="mx-auto flex w-[90%] max-w-[1440px] items-center justify-between">
     <ThemeSwitcher />
     <PackageVersion />
-    <ul class="flex list-none items-center justify-center gap-8">
-      <li>
-        <a
+  
+
+      <ul class="flex list-none items-center justify-center gap-8">
+        <li>
+          <a 
+          style:color={hovering ? 'yellow' : 'blue'}
           data-test-id="discord-btn"
           class="transition duration-200 hover:text-primary-100 active:text-primary-100"
           href="http://discord.eddiehub.org"
           target="_blank"
           rel="noreferrer"
           aria-label="Discord"><i class="fa-brands fa-discord fa-xl" /></a
-        >
-      </li>
-      <li>
-        <a
+          >
+        </li>
+        <li>
+          <a
           data-test-id="github-btn"
           target="_blank"
           rel="noreferrer"
           class="transition duration-200 hover:text-primary-100 active:text-primary-100"
           href="http://github.eddiehub.org"
           aria-label="Github"><i class="fa-brands fa-github fa-xl" /></a
-        >
-      </li>
-    </ul>
+          >
+        </li>
+      </ul>
+
   </div>
 </footer>
