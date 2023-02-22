7cb8d5fd08451011f5f45cde50f94d1b67f85588
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-02-22 07:56:15 +0000

    style: adjust styling of component

--- src/lib/components/timeline.svelte
+++ src/lib/components/timeline.svelte
@@ -4,14 +4,14 @@
 
 <ol class="text-p-large flex flex-col text-sm">
   {#each changes as change}
-    <li class="relative flex pb-8">
+    <li class="relative flex h-12">
       <div class="absolute inset-0 flex h-full w-3 items-center justify-center">
         <div class="pointer-events-none h-full border-[0.5px] border-skin-text-highlight" />
       </div>
       <div
-        class="relative z-10 inline-flex h-3 w-3 flex-shrink-0 items-center justify-center rounded-full bg-skin-primary"
+        class="absolute top-0 left-0 bottom-0 right-0 z-10 my-auto inline-flex h-3 w-3 flex-shrink-0 items-center justify-center rounded-full bg-skin-primary"
       />
-      <div class="flex-grow pl-4">
+      <div class="flex flex-grow items-center pl-5">
         {@html change}
       </div>
     </li>
