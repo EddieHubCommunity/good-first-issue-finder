ea01d5f9e2048c998e599d32cc5d5e8515528c7e
dhruvin <dhruvin.200410116086@gmail.com>
2023-02-23 23:46:40 +0530

    final commit

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,38 +1,34 @@
 <script lang="ts">
   import ThemeSwitcher from './theme-switcher.svelte';
   import PackageVersion from './package-version.svelte';
-  
 </script>
 
 <footer class="mt-auto w-full bg-skin-off-background py-4">
   <div class="mx-auto flex w-[90%] max-w-[1440px] items-center justify-between">
     <ThemeSwitcher />
     <PackageVersion />
-  
 
-      <ul class="flex list-none items-center justify-center gap-8">
-        <li>
-          <a 
-          style:color={hovering ? 'yellow' : 'blue'}
+    <ul class="flex list-none items-center justify-center gap-8">
+      <li>
+        <a
           data-test-id="discord-btn"
           class="transition duration-200 hover:text-[#7289da] active:text-primary-100"
           href="http://discord.eddiehub.org"
           target="_blank"
           rel="noreferrer"
           aria-label="Discord"><i class="fa-brands fa-discord fa-xl" /></a
-          >
-        </li>
-        <li>
-          <a
+        >
+      </li>
+      <li>
+        <a
           data-test-id="github-btn"
           target="_blank"
           rel="noreferrer"
-         class="transition duration-200  hover:text-slate-400 hover:fill-red-300 dark:hover:text-[#fafafa] active:text-primary-100"
+          class="transition duration-200  hover:fill-red-300 hover:text-slate-400 active:text-primary-100 dark:hover:text-[#fafafa]"
           href="http://github.eddiehub.org"
           aria-label="Github"><i class="fa-brands fa-github fa-xl" /></a
-          >
-        </li>
-      </ul>
-
+        >
+      </li>
+    </ul>
   </div>
 </footer>
