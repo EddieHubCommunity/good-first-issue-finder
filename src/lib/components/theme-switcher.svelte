13bb267f3ecb4055ea36973eef18b92d6c7bc0e7
Avinash Gupta <avinashg2052000@gmail.com>
2023-06-14 23:02:44 +0530

    Update theme-switcher.svelte
    Make change in theme-switcher ...... And create only one button to switch light

--- src/lib/components/theme-switcher.svelte
+++ src/lib/components/theme-switcher.svelte
@@ -6,58 +6,36 @@
     let storedTheme = localStorage.getItem('theme');
     if (storedTheme === null || storedTheme === 'system') {
       theme = 'system';
-    } else if (storedTheme === 'dark') {
-      theme = 'dark';
-    } else {
+    } else if (storedTheme === 'light') {
       theme = 'light';
+    } else {
+      theme = 'dark';
     }
   });
+
   const setLight = () => {
-    document.body.classList.replace('dark', 'light');
-    localStorage.setItem('theme', 'light');
-    theme = 'light';
-  };
-  const setDark = () => {
-    document.body.classList.replace('light', 'dark');
-    localStorage.setItem('theme', 'dark');
-    theme = 'dark';
-  };
-  const setSystem = () => {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.body.classList.replace('light', 'dark');
-    } else {
+    if (theme == 'dark') {
       document.body.classList.replace('dark', 'light');
+      localStorage.setItem('theme', 'light');
+      theme = 'light';
+    } else {
+      document.body.classList.replace('light', 'dark');
+      localStorage.setItem('theme', 'dark');
+      theme = 'dark';
     }
-    localStorage.setItem('theme', 'system');
-    theme = 'system';
   };
 </script>
 
 <div data-test-id="theme-switch" class="flex flex-col sm:flex-row">
   <button
-    class="border-b border-black p-2 text-xs dark:border-white sm:border-b-0 sm:border-r md:p-3 md:text-base"
-    title="System Theme"
-    on:click={setSystem}
-    ><i
-      class:active={theme === 'system'}
-      class="fa-solid fa-gear text-skin-off dark:text-skin-text-highlight"
-    /></button
-  >
-  <button
-    class="border-b border-black p-2 text-xs dark:border-white sm:border-b-0 sm:border-r md:p-3 md:text-base"
-    title="Light Theme"
+    class="  text-m border-black p-2 dark:border-white md:p-3 md:text-base"
+    title="Switch light"
     on:click={setLight}
     ><i
       class:active={theme === 'light'}
       class="fa-solid fa-sun text-skin-off dark:text-skin-text-highlight"
-    /></button
-  >
-  <button class="p-2 text-xs md:p-3 md:text-base" title="Dark Theme" on:click={setDark}
-    ><i
-      class:active={theme === 'dark'}
-      class="fa-solid fa-moon text-skin-off dark:text-skin-text-highlight"
-    /></button
-  >
+    />
+  </button>
 </div>
 
 <style lang="postcss">
