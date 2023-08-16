870236041558ffcf7b59474d588f43c5bb37f0da
Akhilesh Raturi <ankitraturi44@gmail.com>
2023-08-16 22:34:04 +0530

    Applied the media query for reduced motion, removed additional css file

--- src/lib/components/index/hero.svelte
+++ src/lib/components/index/hero.svelte
@@ -7,7 +7,13 @@
 </head>
 
 <div class="space-y-8">
-  <h1 class="gradient-flow">Good-first-issue-finder</h1>
+  <h1
+    class="gradient-flow webkit-bg-clip-text mx-auto inline-block max-w-3xl bg-gradient-to-r
+   from-primary-100 via-accent-magenta to-accent-cyan bg-clip-text text-center text-3xl font-bold text-transparent md:text-6xl"
+  >
+    Good-first-issue-finder
+  </h1>
+
   <h2 class="max-w-2xl text-center md:text-xl">
     Your entry point for finding good first issues in the EddieHub Organization and in the community
   </h2>
@@ -15,3 +21,33 @@
     <Link variant="primary" href="/app">Get started</Link>
   </div>
 </div>
+
+<style lang="postcss">
+  .gradient-flow {
+    background-size: 200% 100%;
+    animation: gradientAnimation 2s linear infinite alternate-reverse;
+  }
+
+  @media (prefers-reduced-motion: reduce) {
+    .md\:text-6xl {
+      animation: none;
+      background-size: 100% 100%;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .md\:text-6xl {
+      font-size: 3.75rem;
+      line-height: 1;
+    }
+  }
+
+  @keyframes gradientAnimation {
+    0% {
+      background-position: 0% 50%;
+    }
+    100% {
+      background-position: 100% 50%;
+    }
+  }
+</style>
