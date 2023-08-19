473226b4a190c7af4816e4823aac755ab39d6cea
Gurkirat Singh <tbhaxor@gmail.com>
2023-08-19 22:42:41 +0530

    Move unique tags functionality to `filter.svelte`

--- src/lib/components/filter.svelte
+++ src/lib/components/filter.svelte
@@ -3,7 +3,7 @@
   export let tags: string[];
   export let group: string[];
 
-  $: tags = tags.sort((a, b) => a.localeCompare(b));
+  $: tags = [...new Set(tags)].sort((a, b) => a.localeCompare(b));
 </script>
 
 <form class="mt-8 grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
