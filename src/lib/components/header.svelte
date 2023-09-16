99c329f069936c8ea5f87be641ff8c919cd96359
Love sonkar <82786865+love-sonkar@users.noreply.github.com>
2023-09-16 23:11:46 +0530

    fixes #373 hamburger menu hidden issue in small devices

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -42,7 +42,7 @@
       data-test-id="hamburger-btn"
     >
       <svg
-        class="block h-4 w-4 fill-current dark:hidden"
+        class="block h-4 w-4 dark:hidden"
         viewBox="0 0 20 20"
         xmlns="http://www.w3.org/2000/svg"
       >
