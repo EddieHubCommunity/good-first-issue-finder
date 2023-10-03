b4e35bffa0795500acd4db7e847ae8305f1be07c
Emilia Miller <emiller@future-processing.com>
2023-10-03 21:27:08 +0200

    bugITwhisperer: playwright skeleton tests duplicates removal

--- tests/docs/introduction.spec.ts
+++ tests/docs/introduction.spec.ts
@@ -1,29 +1,18 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { expect, test } from '@playwright/test';
 
-test('docs header element is active', async ({ page }) => {
+test('Docs header element is active', async ({ page }) => {
   // navigate to the docs landing page /docs
   await page.goto('/docs');
+
   // check if the docs link in the page header is having the active class
   await expect(page.getByRole('link', { name: 'Docs' })).toHaveClass(/active/);
 });
 
-test('Correct Page Heading', async ({ page }) => {
+test('Page Heading is correct', async ({ page }) => {
   // navigate to the docs landing page /docs
   await page.goto('/docs');
+
   // check if the heading contains Introduction
   expect(await page.textContent('h1')).toBe('Introduction');
-});
-
-test.fixme(
-  'is the left-side menu containing an element with the same name as the page headline',
-  async ({ page }) => {
-    // navigate to the docs landing page /docs
-    // check if the Introduction element in the left menu is holding the active class
-  },
-);
-
-test.fixme('is the highlighted item in the left-menu is the correct one', async ({ page }) => {
-  // navigate to the docs landing page /docs
-  // check if the Introduction element in the left menu is holding the active class
 });
