8ed9feea1e7e325bd20bfa701a3ae136daf1f050
Emilia Miller <emiller@future-processing.com>
2023-10-08 17:16:03 +0200

    bugITwhisperer: fix test - intro element has the same name as page header

--- tests/docs/introduction.spec.ts
+++ tests/docs/introduction.spec.ts
@@ -17,15 +17,25 @@ test('Page Heading is correct', async ({ page }) => {
   expect(await page.textContent('h1')).toBe('Introduction');
 });
 
-test.fixme('is the highlighted item in the left-menu is the correct one', async ({ page }) => {
+test('Introduction element is highlighted as orange', async ({ page }) => {
   // navigate to the docs landing page /docs
-  // check if the Introduction element in the left menu is highlighted
+  await page.goto('/docs');
+
+  // check the Introduction element has orange color
+  const introElement = page.getByRole('link', { name: 'Docs' });
+  const introElementColor = await introElement.evaluate((element) =>
+    window.getComputedStyle(element).getPropertyValue('--primary-100'),
+  );
+  await expect(introElementColor).toEqual('#ff5a00');
 });
 
-test.fixme(
-  'is the left-side menu containing an element with the same name as the page headline',
-  async ({ page }) => {
-    // navigate to the docs landing page /docs
-    // check if both elements in the left menu and page header are the same and say 'Introduction'
-  },
-);
+test.only('Introduction element has the same name as the page headline', async ({ page }) => {
+  // navigate to the docs landing page /docs
+  await page.goto('/docs');
+
+  // check if both elements in the left menu and page header are the same and say 'Introduction'
+  const pageHeadlineName = await page.textContent('h1');
+  const introElement = await page.locator('.space-y-4 .active').innerHTML();
+  const introElementName = introElement.toString();
+  expect(introElementName).toBe(pageHeadlineName);
+});
