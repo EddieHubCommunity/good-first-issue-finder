cf6b370ec27ed229685a7e5830b7f8b5ffefd5df
me <lopezbeto305@gmail.com>
2024-06-19 18:10:55 -0400

    fix: Centered <h2> tag by adding mx-auto class

--- src/lib/components/index/hero.svelte
+++ src/lib/components/index/hero.svelte
@@ -8,7 +8,7 @@
   >
     Good-first-issue-finder
   </h1>
-  <h2 class="max-w-2xl text-center md:text-xl">
+  <h2 class="mx-auto max-w-2xl text-center md:text-xl">
     Your entry point for finding good first issues in the EddieHub Organization and in the community
   </h2>
   <div class="flex justify-center">
